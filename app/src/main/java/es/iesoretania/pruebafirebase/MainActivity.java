e0a80324166f29e1d0afa311fe876cd915d33635
Manu Villar <mjvillar@iesoretania.es>
2021-11-10 22:33:00 +0100

    Añadido binding de vistas y parámetro String al método MuestraAlerta().

--- app/src/main/java/es/iesoretania/pruebafirebase/MainActivity.java
+++ app/src/main/java/es/iesoretania/pruebafirebase/MainActivity.java
@@ -16,77 +16,81 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import es.iesoretania.pruebafirebase.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
-    EditText email, contraseña;
-    Button botonregistrar, botonacceder;
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setTitle("Pantalla de Autenticación");
 
-        email = findViewById(R.id.EditTextEmail);
-        contraseña = findViewById(R.id.EditTextPassword);
-        botonregistrar = findViewById(R.id.botonRegistrar);
-        botonacceder = findViewById(R.id.botonAcceder);
-
-        botonregistrar.setOnClickListener(new View.OnClickListener() {
+        binding.botonRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!email.getText().toString().isEmpty() && !contraseña.getText().toString().isEmpty()){
+                if (!binding.EditTextEmail.getText().toString().isEmpty()
+                        && !binding.EditTextPassword.getText().toString().isEmpty()){
                     FirebaseAuth.getInstance()
-                            .createUserWithEmailAndPassword(email.getText().toString(),contraseña.getText().toString())
+                            .createUserWithEmailAndPassword(binding.EditTextEmail.getText().toString(),
+                                    binding.EditTextPassword.getText().toString())
                             .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Intent intentHome = new Intent(v.getContext(), HomeActivity.class);
-                                        intentHome.putExtra("email", email.getText().toString());
+                                        intentHome.putExtra("email", binding.EditTextEmail.getText().toString());
                                         intentHome.putExtra("proveedor", "EMAIL-PASSWORD");
 
                                         startActivity(intentHome);
                                     } else {
-                                        muestraAlerta();
+                                        muestraAlerta("Error en la autenticación");
                                     }
                                 }
                             });
+                }else{
+                    muestraAlerta("Introduce valores en todos los campos");
                 }
             }
         });
 
-        botonacceder.setOnClickListener(new View.OnClickListener() {
+        binding.botonAcceder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!email.getText().toString().isEmpty() && !contraseña.getText().toString().isEmpty()){
+                if (!binding.EditTextEmail.getText().toString().isEmpty()
+                        && !binding.EditTextPassword.getText().toString().isEmpty()){
                     FirebaseAuth.getInstance()
-                            .signInWithEmailAndPassword(email.getText().toString(),contraseña.getText().toString())
+                            .signInWithEmailAndPassword(binding.EditTextEmail.getText().toString(),
+                                    binding.EditTextPassword.getText().toString())
                             .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Intent intentHome = new Intent(v.getContext(), HomeActivity.class);
-                                        intentHome.putExtra("email", email.getText().toString());
+                                        intentHome.putExtra("email", binding.EditTextEmail.getText().toString());
                                         intentHome.putExtra("proveedor", "EMAIL-PASSWORD");
 
                                         startActivity(intentHome);
                                     } else {
-                                        muestraAlerta();
+                                        muestraAlerta("Error de autenticación");
                                     }
                                 }
                             });
+                }else {
+                    muestraAlerta("Introduce valores en todos los campos");
                 }
             }
         });
     }
 
-    private void muestraAlerta() {
+    private void muestraAlerta(String s) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Error");
-        builder.setMessage("Se ha producido un error autenticando al usuario");
+        builder.setMessage(s);
         builder.setPositiveButton("Aceptar", null);
         AlertDialog dialogo = builder.create();
         dialogo.show();

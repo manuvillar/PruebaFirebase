8d05c4588824803258184d565937cde96dc4c190
Manu Villar <mjvillar@iesoretania.es>
2021-11-11 23:39:28 +0100

    Añadido botón y funcionalidad de login con cuenta de Google.

--- app/src/main/java/es/iesoretania/pruebafirebase/MainActivity.java
+++ app/src/main/java/es/iesoretania/pruebafirebase/MainActivity.java
@@ -1,26 +1,32 @@
 package es.iesoretania.pruebafirebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import es.iesoretania.pruebafirebase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAnalytics mFirebaseAnalytics;
     private ActivityMainBinding binding;
+    final Integer GOOGLE_SIGN_IN=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +91,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        binding.botonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(v.getContext(), gso);
+                mGoogleSignInClient.signOut();
+
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+            }
+        });
     }
 
     private void muestraAlerta(String s) {
@@ -94,5 +116,38 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("Aceptar", null);
         AlertDialog dialogo = builder.create();
         dialogo.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                if (account != null){
+                    AuthCredential credenciales = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credenciales).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intentHome = new Intent(getBaseContext(), HomeActivity.class);
+                                intentHome.putExtra("email", account.getEmail());
+                                intentHome.putExtra("proveedor", "CUENTA-GOOGLE");
+
+                                startActivity(intentHome);
+                            } else {
+                                muestraAlerta("Error de autenticación");
+                            }
+                        }
+                    });
+                }
+            } catch (ApiException e) {
+                muestraAlerta("Error en login con Google");
+            }
+        }
+
     }
 }
